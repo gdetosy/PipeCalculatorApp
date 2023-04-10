b55ddee5397c30ef8ddeee828a692f57bc2c1a01
gdetosy <tosyjdm@gmail.com>
2023-04-10 21:33:21 +0300

    protocol

--- PipeCalculatorApp/PipeCalculatorApp/Controller/ViewController.swift
+++ PipeCalculatorApp/PipeCalculatorApp/Controller/ViewController.swift
@@ -35,13 +35,21 @@ final class ViewController: UIViewController {
         about()
     }
     
-    @IBAction func diameterTextField(_ sender: Any) {}
+    @IBAction func diameterTextField(_ sender: Any) {
+        massa()
+    }
     
-    @IBAction func thicknesTextField(_ sender: Any) {}
+    @IBAction func thicknesTextField(_ sender: Any) {
+        massa()
+    }
     
-    @IBAction func lengthTextField(_ sender: Any) {}
+    @IBAction func lengthTextField(_ sender: Any) {
+        massa()
+    }
     
-    @IBAction func weightTextField(_ sender: Any) {}
+    @IBAction func weightTextField(_ sender: Any) {
+        massa()
+    }
     
     @IBAction func segmentControl(_ sender: Any) {}
     
@@ -50,31 +58,31 @@ final class ViewController: UIViewController {
         self.hideKeyboardWhenTappedAround()
     }
 
-    private func massa() {
-        guard let diametr = Float(diameterTf.text!.replacingOccurrences(of: ",", with: ".")),
-              let stenka = Float(thicknesTf.text!.replacingOccurrences(of: ",", with: ".")),
-              let metraj = Float(lengthTf.text!.replacingOccurrences(of: ",", with: "."))
-        else { return }
-        let massa1 = ((diametr - stenka) * stenka * 0.02466 * metraj) / 1000
-        let heightMetrs = ((diametr - stenka) * stenka * 0.02466 * 1) / 1000
-        let kg = (diametr - stenka) * stenka * 0.02466
-        meterLabel.text = "1 m weight = \(round(heightMetrs * 100000) / 100000) t = \(kg) kg."
-//        heightMetr.animation = Animations.fadeIn.rawValue
-//        heightMetr.animate()
-        weightTf.text = "\(round(massa1 * 100000) / 100000)"
-    }
+//    private func massa() {
+//        guard let diametr = Float(diameterTf.text!.replacingOccurrences(of: ",", with: ".")),
+//              let stenka = Float(thicknesTf.text!.replacingOccurrences(of: ",", with: ".")),
+//              let metraj = Float(lengthTf.text!.replacingOccurrences(of: ",", with: "."))
+//        else { return }
+//        let massa1 = ((diametr - stenka) * stenka * 0.02466 * metraj) / 1000
+//        let heightMetrs = ((diametr - stenka) * stenka * 0.02466 * 1) / 1000
+//        let kg = (diametr - stenka) * stenka * 0.02466
+//        meterLabel.text = "1 m weight = \(round(heightMetrs * 100000) / 100000) t = \(kg) kg."
+////        heightMetr.animation = Animations.fadeIn.rawValue
+////        heightMetr.animate()
+//        weightTf.text = "\(round(massa1 * 100000) / 100000)"
+//    }
 
-    private func length() {
-        guard let diametr = Float(diameterTf.text!.replacingOccurrences(of: ",", with: ".")),
-              let stenka = Float(thicknesTf.text!.replacingOccurrences(of: ",", with: ".")),
-              let massa = Float(lengthTf.text!.replacingOccurrences(of: ",", with: "."))
-        else { return }
-        let metraj1 = massa * 1000 / ((diametr - stenka) * 0.0246 * stenka)
-        let heightMetrs = ((diametr - stenka) * stenka * 0.02466 * 1) / 1000
-        let kg = (diametr - stenka) * stenka * 0.02466
-        weightTf.text = "\(round(metraj1 * 100000) / 100000)"
-        meterLabel.text = "1 m weight = \(round(heightMetrs * 100000) / 100000) t = \(kg) kg."
-    }
+//    private func length() {
+//        guard let diametr = Float(diameterTf.text!.replacingOccurrences(of: ",", with: ".")),
+//              let stenka = Float(thicknesTf.text!.replacingOccurrences(of: ",", with: ".")),
+//              let massa = Float(lengthTf.text!.replacingOccurrences(of: ",", with: "."))
+//        else { return }
+//        let metraj1 = massa * 1000 / ((diametr - stenka) * 0.0246 * stenka)
+//        let heightMetrs = ((diametr - stenka) * stenka * 0.02466 * 1) / 1000
+//        let kg = (diametr - stenka) * stenka * 0.02466
+//        weightTf.text = "\(round(metraj1 * 100000) / 100000)"
+//        meterLabel.text = "1 m weight = \(round(heightMetrs * 100000) / 100000) t = \(kg) kg."
+//    }
     private func about() {
         if lengthLabel.text == "Длина, м " { alert() }
         else { alert1() }
