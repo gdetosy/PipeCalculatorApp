bbcab3110f3a0cdd2a331cc9c2c33ff10da17ad8
gdetosy <tosyjdm@gmail.com>
2023-04-10 21:26:11 +0300

    no message

--- PipeCalculatorApp/PipeCalculatorApp/Controller/ViewController.swift
+++ PipeCalculatorApp/PipeCalculatorApp/Controller/ViewController.swift
@@ -21,9 +21,14 @@ class ViewController: UIViewController {
     @IBOutlet var znPipeLabel: UILabel!
    
     @IBOutlet var diameterTf: UITextField!
+    
     @IBOutlet var thicknesTf: UITextField!
+    
     @IBOutlet var lengthTf: UITextField!
+    
     @IBOutlet var weightTf: UITextField!
+    
+    @IBOutlet weak var questions: UIImageView!
     
     @IBAction func diameterTextField(_ sender: Any) {}
     
@@ -39,6 +44,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
     private func massa() {
         guard let diametr = Float(diameterTf.text!.replacingOccurrences(of: ",", with: ".")),
               let stenka = Float(thicknesTf.text!.replacingOccurrences(of: ",", with: ".")),
@@ -50,26 +56,18 @@ class ViewController: UIViewController {
         meterLabel.text = "1 m weight = \(round(heightMetrs * 100000) / 100000) t = \(kg) kg."
 //        heightMetr.animation = Animations.fadeIn.rawValue
 //        heightMetr.animate()
-        heightTextField.text = "\(round(massa1 * 100000) / 100000)"
+        weightTf.text = "\(round(massa1 * 100000) / 100000)"
     }
+
     private func length() {
-        guard let diametr = Float(diametrTextField.text!.replacingOccurrences(of: ",", with: ".")),
-              let stenka = Float(tolshinaTextField.text!.replacingOccurrences(of: ",", with: ".")),
-              let massa = Float(dlinaTextField.text!.replacingOccurrences(of: ",", with: "."))
+        guard let diametr = Float(diameterTf.text!.replacingOccurrences(of: ",", with: ".")),
+              let stenka = Float(thicknesTf.text!.replacingOccurrences(of: ",", with: ".")),
+              let massa = Float(lengthTf.text!.replacingOccurrences(of: ",", with: "."))
         else { return }
         let metraj1 = massa * 1000 / ((diametr - stenka) * 0.0246 * stenka)
         let heightMetrs = ((diametr - stenka) * stenka * 0.02466 * 1) / 1000
         let kg = (diametr - stenka) * stenka * 0.02466
-        heightTextField.text = "\(round(metraj1 * 100000) / 100000)"
-       meterLabel.text = "1 m weight = \(round(heightMetrs * 100000) / 100000) t = \(kg) kg."
+        weightTf.text = "\(round(metraj1 * 100000) / 100000)"
+        meterLabel.text = "1 m weight = \(round(heightMetrs * 100000) / 100000) t = \(kg) kg."
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
