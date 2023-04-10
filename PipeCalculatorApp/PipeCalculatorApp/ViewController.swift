483e31977b442433e55dbc9108f1f78800d460a9
gdetosy <tosyjdm@gmail.com>
2023-04-10 20:35:25 +0300

    no message

--- PipeCalculatorApp/PipeCalculatorApp/ViewController.swift
+++ PipeCalculatorApp/PipeCalculatorApp/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var checkMarkD: UIImageView!
+    
+    @IBOutlet var checkMarkS: UIImageView!
+    
+    @IBOutlet var checkMarkL: UIImageView!
+    
+    @IBOutlet var checkMarkH: UILabel!
+   
+    @IBOutlet var meterLabel: UILabel!
+    
+    @IBOutlet var znPipeLabel: UILabel!
+    
+    @IBAction func diameterTextField(_ sender: Any) {}
+    
+    @IBAction func thicknesTextField(_ sender: Any) {}
+    
+    @IBAction func lengthTextField(_ sender: Any) {}
+    
+    @IBAction func weightTextField(_ sender: Any) {}
+    
+    @IBAction func segmentControl(_ sender: Any) {}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
